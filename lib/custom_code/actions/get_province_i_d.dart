beeb0cc512fb497c470eab77d1c8470e6c61f5a8
Anusorn Kongthong <>
2023-06-15 09:24:34 +0700

    a

--- lib/custom_code/actions/get_province_i_d.dart
+++ lib/custom_code/actions/get_province_i_d.dart
@@ -4,9 +4,12 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'index.dart'; // Imports other custom actions
 import 'package:flutter/material.dart';
+
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
 Future<int> getProvinceID(String? province) async {
   // Add your function code here!
+  var rs = await FirebaseFirestore.instance.collection("province").where("name", isEqualTo: province).get();
+  return rs.docs[0].data()["id"];
 }
